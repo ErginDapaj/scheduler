71c3147fa87fd5d5524bdd52dfb74bd89e97eb4a
remixor <ergindapaj081@gmail.com>
2022-05-07 21:27:02 +0200

    working on the API (cool stuff)

--- server/index.js
+++ server/index.js
@@ -6,7 +6,40 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.get("/api", (req, res) => {
-    res.json({ message: "Hello from server!" });
+    function getDaysInMonth(year, month) {
+        return new Date(year, month, 0).getDate();
+    }
+
+    const date = new Date();
+    const currentYear = date.getFullYear();
+    const currentMonth = date.getMonth() + 1;
+
+    // 👇️ Current Month
+    const thisMonth = getDaysInMonth(currentYear, currentMonth);
+    console.log(thisMonth);
+    let workers = [
+        {
+            "workerName": "Grainger",
+            "shift": 3,
+            "daysOff": [6,9]
+        },
+        {
+            "workerName": "Grrrrrrr",
+            "shift": 1,
+            "daysOff": [6,9]
+        },
+        {
+            "worker": "Idk",
+            "shift": 2,
+            "daysOff": [6,9]
+        }
+    ]
+    workers.sort((a, b) => (a.shift) - (b.shift));
+    console.log(workers)
+    let month = thisMonth
+    for (let i = 0; i < month; i++) {
+    }
+    res.json({ message: workers });
 });
 
 app.listen(PORT, () => {
