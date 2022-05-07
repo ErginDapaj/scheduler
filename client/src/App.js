af6dcae4bfd24264134730cb6099a9a9e963145b
remixor <ergindapaj081@gmail.com>
2022-05-07 17:34:26 +0200

    fixed something at least it will be here just for a bit

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,25 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import 'react-tabulator/lib/styles.css'; // required styles
-import 'react-tabulator/lib/css/tabulator.min.css'; // theme
-import { ReactTabulator } from 'react-tabulator';
+import Schedule from 'react-schedule-job'
+import 'react-schedule-job/dist/index.css'
+const function_1 = () => {};
+  
+const function_2 = () => {};
+  
+const jobs = [
+  {
+    fn: function_1,
+    id: '1',
+    schedule: '* * * * *',
+  }
+,
+  {
+    fn: function_2,
+    id: '2',
+    schedule: '4 * 8,2,10,17,21,14,19 4,1 *',
+  }
+]
 
 function App() {
   const [data, setData] = React.useState(null);
@@ -19,6 +35,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>{!data ? "Loading..." : data}</p>
+        <Schedule 
+        jobs={jobs}
+        timeZone='UTC'
+        dashboard={{ hidden: false }}
+      />
       </header>
     </div>
   );
